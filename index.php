ec7033e0ed75113a57a745fcdbb710352d66a3b4
Fez Denver <fezdenver@gmail.com>
2017-08-16 15:38:27 -0400

    Main Pages
    Added Main Page and Upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+ 
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file" /><br />
+    <input type="submit" name="submit" />
+</form>
+
+<?php
+$dirname = "photos/";
+$images = glob($dirname."*.png");
+
+foreach($images as $image) {
+    echo '<img src="'.$image.'" /><br />';
+}
+?>
+
+<?php
+    if(isset($_GET["img"]))
+    {
+        $n=$_GET["img"];
+        echo "<img src='photos/".$n."'>";
+    }
+?>
+</body>
+</html>
